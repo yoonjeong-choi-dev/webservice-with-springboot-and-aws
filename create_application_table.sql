49317ba11631579585c387851e0a372b3484b500
yjchoi7166 <yjchoi7166@urbanbase.com>
2021-07-04 22:23:54 +0900

    [chore] user 및 post 테이블 생성 sql 구문 추가

--- create_application_table.sql
+++ create_application_table.sql
@@ -1,4 +1,26 @@
 use yjchoi_springboot_webservice;
+
+create table posts (
+	id bigint not null auto_increment, 
+	created_date datetime, 
+	modified_date datetime, 
+	author varchar(255), 
+	content TEXT not null, 
+	title varchar(500) not null,
+	primary key (id)
+) engine=InnoDB;
+
+create table user (
+	id bigint not null auto_increment, 
+	created_date datetime, 
+	modified_date datetime, 
+	email varchar(255) not null, 
+	name varchar(255) not null, 
+	picture varchar(255), 
+	role varchar(255) not null, 
+	primary key (id)
+) engine=InnoDB;
+
 CREATE TABLE SPRING_SESSION (
 	PRIMARY_ID CHAR(36) NOT NULL,
 	SESSION_ID CHAR(36) NOT NULL,
